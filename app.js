66b30d8c9108b8381ba1ce44e3c94de8896b813f
Kelvin Kni <keltheceo@gmail.com>
2019-04-18 11:11:31 -0700

    add some modifications

--- app.js
+++ app.js
@@ -1,18 +1,35 @@
 const express = require('express')
 const path = require('path')
+const ejs = require('ejs')
 
 const app = express()
 
-/** @desc: tells Express that any file ending in (.ejs)
- * should be rendered with the (ejs) engine/package
- * */
-app.set('view engine', 'ejs')
-// lets Express know where the views folder is
-app.set('views', path.resolve(__dirname, 'views'))
+app.locals.appName = 'Song Lyrics'
 
-// a hit to this endPoint will yield the index.ejs code
-app.get('/', (req, res) => {
-    res.render('index')
+app.set('view engine', 'jade')
+app.set('views', path.resolve(__dirname, 'views'))
+app.engine('html', ejs.renderFile)
+
+app.use((req, res, next) => {
+    res.locals.userAgent = req.headers['user-agent']
+    next()
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        currentUser: 'india-arie123'
+    })
+})
+
+app.get('/contact', (req, res) => {
+    res.render('contact.ejs')
+})
+
+app.use((req, res) => {
+    res.staus(404)
+    res.render('404.html', {
+        urlAttempted: req.url
+    })
 })
 
 app.listen(3000)
